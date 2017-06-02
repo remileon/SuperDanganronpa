f6281057a2ae52af017d04496e23365af5e50152
Haonan Li <haonanli@6tiantian.com>
2017-06-02 15:39:10 +0800

    commit！

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -13,17 +13,17 @@ public class Bullet : MonoBehaviour {
     }
 	// Use this for initialization
 	void Start () {
-	    
+        float deltaX = -Mathf.Sin(trans.rotation.eulerAngles.z / 360f * 2 * Mathf.PI);
+        float deltaY = Mathf.Cos(trans.rotation.eulerAngles.z / 360f * 2 * Mathf.PI);
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(deltaX, deltaY) * speed;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        float distance = (Time.deltaTime * speed);
-        float deltaX = -Mathf.Sin(trans.rotation.eulerAngles.z / 360f * 2 * Mathf.PI) * distance;
-        float deltaY = Mathf.Cos(trans.rotation.eulerAngles.z / 360f * 2 * Mathf.PI) * distance;
-        trans.position += new Vector3(deltaX, deltaY, 0);
-        if (Mathf.Abs(trans.position.x) > 20 || Mathf.Abs(trans.position.y) > 10) {
-            Destroy(gameObject);
-        }
+        //float distance = (Time.deltaTime * speed);
+        //float deltaX = -Mathf.Sin(trans.rotation.eulerAngles.z / 360f * 2 * Mathf.PI) * distance;
+        //float deltaY = Mathf.Cos(trans.rotation.eulerAngles.z / 360f * 2 * Mathf.PI) * distance;
+        //trans.position += new Vector3(deltaX, deltaY, 0);
 	}
 }
---
+++ Assets/DestoryByBoundary.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestoryByBoundary : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("enter");
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        Debug.Log("stay");
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        Debug.Log("exit");
+        Destroy(other.gameObject);
+    }
+}
