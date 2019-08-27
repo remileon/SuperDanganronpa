d8a13f61cd7e7e627623dd52c28bd638c4b7fc04
jollyjolteon <443972553@qq.com>
2019-08-27 23:53:59 +0800

    animator

---
+++ Assets/avg/Option.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Option : MonoBehaviour
+{
+    private Animator animator;
+    private static readonly int hovered = Animator.StringToHash("hovered");
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            animator.SetBool(hovered, true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            animator.SetBool(hovered, false);
+        }
+    }
+}
